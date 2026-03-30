81a2637d7fbc3a525922f55dbd58c9898ac45aa1
orangeem1219 <orangeem1219@gmail.com>
2026-03-30 21:51:13 +0900

    2026-03-30 수업 추가4

---
+++ styled/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ButtonSample from './components/ButtonSample'
+import MainPage from './components/MainPage'
+
+function App() {
+
+  return (
+    <>
+    <ButtonSample />
+    <MainPage />
+      
+    </>
+  )
+}
+
+export default App
